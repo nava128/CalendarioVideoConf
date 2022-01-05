6dae74fb8b7b665ace96626d472fff767247c62e
FlutLab.io <>
2022-01-05 16:01:39 +0000

    Autosave

--- lib/ui/ejemplo_DatePicker.dart
+++ lib/ui/ejemplo_DatePicker.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:nava/ui/cls_colores.dart';
+import 'package:nava/ui/widget_timepicker_textfield.dart';
 
 class FlutterDatePickerExample extends StatelessWidget {
   final ValueNotifier<DateTime?> dateSub = ValueNotifier(null);
@@ -173,57 +173,5 @@ class FlutterDatePickerExample extends StatelessWidget {
     );
   }
 
-  String convertDate(DateTime dateTime) {
-    return DateFormat('dd/MM/yyyy').format(dateTime);
-  }
-
-  String longDate(DateTime dateTime) {
-    return DateFormat('EEE, MMM d, yyy').format(dateTime);
-  }
-
-  String convertTime(TimeOfDay timeOfDay) {
-    DateTime tempDate = DateFormat('hh:mm').parse(
-        timeOfDay.hour.toString() + ':' + timeOfDay.minute.toString());
-    var dateFormat = DateFormat('h:mm a');
-    return dateFormat.format(tempDate);
-  }
-
-
-  Widget buildDateTimePicker(String data) {
-    return ListTile(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-        side: const BorderSide(color: AppColors.eggPlant, width: 1.5),
-      ),
-      title: Text(data),
-      trailing: const Icon(
-        Icons.calendar_today,
-        color: AppColors.eggPlant,
-      ),
-    );
-  }
-
-  Widget buildTextField(
-      {String? hint, required TextEditingController controller}) {
-    return TextField(
-      controller: controller,
-      textCapitalization: TextCapitalization.words,
-      decoration: InputDecoration(
-        labelText: hint ?? '',
-        focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: AppColors.eggPlant, width: 1.5),
-          borderRadius: BorderRadius.circular(
-            10.0,
-          ),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: AppColors.eggPlant, width: 1.5),
-          borderRadius: BorderRadius.circular(
-            10.0,
-          ),
-        ),
-      ),
-    );
-  }
  
 }
